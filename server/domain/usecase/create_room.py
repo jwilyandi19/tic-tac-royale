1c722f424848c8aeba2c33f0a7b1c12c09c90a2b
Rendoru <maulana.firman56@gmail.com>
2019-11-30 15:28:12 +0700

    Add UseCase create room

---
+++ server/domain/usecase/create_room.py
@@ -0,0 +1,16 @@
+import server.domain.model.tictactoe as tttMod
+import server.domain.model.room as roomMod
+import asyncio.locks as lockMod
+
+class CreateRoom:
+
+    def __init__(self, ticTacToe: tttMod.TicTacRoyale, lock: lockMod.Lock):
+        self.tictactoe = ticTacToe
+        self.lock = lock
+
+    def Create(self) -> roomMod.Room:
+        self.lock.acquire()
+        room = self.tictactoe.createRoom()
+        self.lock.release()
+        return room
+
