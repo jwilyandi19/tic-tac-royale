cc75349f9252f33038099825ef21edb8e2a10440
Jason Wilyandi <jasonwilyandi@yahoo.co.id>
2019-11-30 16:58:20 +0700

    update list pawn by room +  update place pawn

--- server/domain/usecase/list_pawn_by_room.py
+++ server/domain/usecase/list_pawn_by_room.py
@@ -1,11 +1,16 @@
 import server.domain.model.room as roomMod
 import server.domain.model.pawn as pawnMod
+import server.domain.model.tictactoe as tttMod
 
 import typing as typ
 
 class ListPawnByRoom:
+
+    def __init__(self, ticTacToe: tttMod.TicTacRoyale):
+        self.tictactoe = ticTacToe
+
     def execute(self,code) -> typ.Sequence[pawnMod.Pawn]:
-        code =
-        pawns = roomMod.Room.getPawns()
+        room = self.tictactoe.findRoom(code)
+        pawns = room.Room.getPawns()
 
         return pawns
--- server/domain/usecase/place_pawn.py
+++ server/domain/usecase/place_pawn.py
@@ -1,20 +1,27 @@
 import server.domain.model.pawn as pawnMod
 import server.domain.model.location as locMod
 import server.domain.model.room as roomMod
-import server.domain.model.tictactoe as tictactoeMod
+import server.domain.model.tictactoe as tttMod
+import asyncio.locks as lockMod
+
 
 class PlacePawn:
+    def __init__(self, ticTacToe: tttMod.TicTacRoyale, lock: lockMod.Lock):
+        self.tictactoe = ticTacToe
+        self.lock = lock
+
     def execute(self,x_coord,y_coord,code):
         location = locMod.Location(x_coord,y_coord)
-        room = roomMod.Room(code)
+        room = tictactoe.findRoom(code)
         pawn_type = room.getAvailableSpot()
 
+        self.lock.acquire()
         pawn = pawnMod.Pawn(pawn_type,location)
-
-        if(not tictactoeMod.TicTacRoyale.hasPawn(location)):
+        if(not tictactoe.hasPawn(location)):
             room.addPawn(pawn)
+            self.lock.release()
         else:
-            raise Exception("Pawn has been placed")
+            self.lock.release()
         
 
 
