5f7acbba7a156694308568e9c718bf191495f933
Jason Wilyandi <jasonwilyandi@yahoo.co.id>
2019-11-30 15:16:40 +0700

    revised place pawn use case

--- server/domain/usecase/place_pawn.py
+++ server/domain/usecase/place_pawn.py
@@ -1,14 +1,21 @@
 import server.domain.model.pawn as pawnMod
 import server.domain.model.location as locMod
+import server.domain.model.room as roomMod
+import server.domain.model.tictactoe as tictactoeMod
 
 class PlacePawn:
-    def execute(self,x_coord,y_coord,turn):
-        pawn_type = ""
+    def execute(self,x_coord,y_coord,code):
         location = locMod.Location(x_coord,y_coord)
-        if turn == 0:
-            pawn_type = pawnMod.PawnType.X
+        room = roomMod.Room(code)
+        pawn_type = room.getAvailableSpot()
+
+        pawn = pawnMod.Pawn(pawn_type,location)
+
+        if(not tictactoeMod.TicTacRoyale.hasPawn(location)):
+            room.addPawn(pawn)
         else:
-            pawn_type = pawnMod.PawnType.O
+            raise Exception("Pawn has been placed")
         
-        pawnMod.Pawn(pawn_type,location)
+
+
 
